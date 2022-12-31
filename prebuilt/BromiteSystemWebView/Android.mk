0b47d7deeea8930f341add1a35ceb220b87dda66
flame-0 <fl4m3@pm.me>
2022-12-31 19:41:03 +0800

    Add LOCAL_REQUIRED_MODULES for BromiteSystemWebView

--- prebuilt/BromiteSystemWebView/Android.mk
+++ prebuilt/BromiteSystemWebView/Android.mk
@@ -5,7 +5,9 @@ LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE := BromiteSystemWebView
 LOCAL_MODULE_CLASS := APPS
 LOCAL_OVERRIDES_PACKAGES := webview
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PRODUCT_MODULE := true
+LOCAL_REQUIRED_MODULES := \
+	libwebviewchromium_loader \
+	libwebviewchromium_plat_support
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 include $(BUILD_PREBUILT)
