35df0b2ce8d6c097a8a19fed2808e294d94d4e8d
flame-0 <fl4m3@pm.me>
2023-01-01 02:23:31 +0800

    Add LOCAL_DEX_PREOPT to BromiteSystemWebView

--- prebuilt/BromiteSystemWebView/Android.mk
+++ prebuilt/BromiteSystemWebView/Android.mk
@@ -2,12 +2,10 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_DEX_PREOPT := false
 LOCAL_MODULE := BromiteSystemWebView
 LOCAL_MODULE_CLASS := APPS
 LOCAL_OVERRIDES_PACKAGES := webview
 LOCAL_PRODUCT_MODULE := true
-LOCAL_REQUIRED_MODULES := \
-	libwebviewchromium_loader \
-	libwebviewchromium_plat_support
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 include $(BUILD_PREBUILT)
