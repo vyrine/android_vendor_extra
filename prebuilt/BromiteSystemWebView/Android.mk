49e566328a69de5141fe0300ef2f01ff45bb59dd
flame-0 <fl4m3@pm.me>
2023-01-01 21:55:08 +0800

    another test fix for bromite

--- prebuilt/BromiteSystemWebView/Android.mk
+++ prebuilt/BromiteSystemWebView/Android.mk
@@ -1,13 +1,15 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_DEX_PREOPT := false
 LOCAL_MODULE := BromiteSystemWebView
 LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TARGET_ARCH := arm64
 LOCAL_OVERRIDES_PACKAGES := webview
+LOCAL_PREBUILT_JNI_LIBS_arm64 := @lib/arm64-v8a/libwebviewchromium.so
 LOCAL_PRODUCT_MODULE := true
+LOCAL_REQUIRED_MODULES := \
+        libwebviewchromium_loader \
+        libwebviewchromium_plat_support
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 include $(BUILD_PREBUILT)
