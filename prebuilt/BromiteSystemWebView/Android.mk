36d2a01790d4102cbaa72ca30449e3a003f2467d
flame-0 <fl4m3@pm.me>
2023-01-02 13:42:17 +0800

    more tests

--- prebuilt/BromiteSystemWebView/Android.mk
+++ prebuilt/BromiteSystemWebView/Android.mk
@@ -1,20 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := BromiteSystemWebView
-ifeq ($(TARGET_CPU_ABI), arm64-v8a)
-    LOCAL_SRC_FILES := arm64_SystemWebView.apk
-else
-    LOCAL_SRC_FILES := arm_SystemWebView.apk
-endif
-LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE := BromiteSystemWebView
+LOCAL_MODULE_CLASS := APPS
 LOCAL_MULTILIB := both
 LOCAL_OVERRIDES_PACKAGES := webview
+LOCAL_PREBUILT_JNI_LIBS_arm := @lib/armeabi-v7a/libwebviewchromium.so
+LOCAL_PREBUILT_JNI_LIBS_arm64 := @lib/arm64-v8a/libwebviewchromium.so
 LOCAL_PRODUCT_MODULE := true
 LOCAL_REQUIRED_MODULES := \
     libwebviewchromium_loader \
     libwebviewchromium_plat_support
-LOCAL_PREBUILT_JNI_LIBS_arm := @lib/armeabi-v7a/libwebviewchromium.so
-LOCAL_PREBUILT_JNI_LIBS_arm64 := @lib/arm64-v8a/libwebviewchromium.so
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
